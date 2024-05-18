c1834ed0199bb77d5b95a30d4583312443366b8b
hxriif <zaidbnuarifc@gmail.com>
2024-05-18 12:27:52 +0530

    changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Navbar from './Navbar'
+import HomeF from './Home_f'
+import HomeS from './Home_s'
+
+
+
 
 function Home() {
-  return (
-    <div>
-      <h1>THis is Home page</h1>
-    </div>
-  )
+    return (
+        <div className='h-screen w-screen '>
+            <div className='h-[7rem] '>
+                <Navbar />
+            </div>
+            <div className='w-screen'>
+               <HomeF/>
+            </div>
+            <div className='w-screen'>
+            <HomeS/>
+            </div>
+
+        </div>
+    )
 }
 
 export default Home
