da87cc3947718596906bb11f93ff3fb4cee32080
hxriif <zaidbnuarifc@gmail.com>
2024-05-20 09:34:59 +0530

    changes

--- src/Components/Home_s.jsx
+++ src/Components/Home_s.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import bg from '../images/bg.jpg'
+import { useNavigate } from 'react-router-dom'
+
 
 
 function HomeS() {
+
+  const navigate=useNavigate()
   return (
-    <div className='w-screen h-[15rem] bg-cover bg-center flex items-center justify-center flex-col  font-extrabold text-white' style={{ backgroundImage: `url(${bg})` }}>
+    <div className='w-screen h-[25rem]  bg-cover bg-center flex items-center justify-center flex-col  font-extrabold text-white' style={{ backgroundImage: `url(${bg})` }}>
     <h1>Keeping more than 100 years of</h1>
     <h1>motoring history alive.</h1>
     <button className='text-white mt-3 bg-gray-700 p-3 items-center flex rounded-lg'>JOIN OUR CLUB</button>
-</div>
+   </div>
+
   )
 }
 
