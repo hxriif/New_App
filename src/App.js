39b5e8cecea0ab44d5dfbf4c3d86207f9c059cb7
hxriif <zaidbnuarifc@gmail.com>
2024-05-20 09:34:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import Home from './Components/Home';
+import Gallery from './Components/Gallery';
+import {CarProvider} from './context/CarContext'
 
 function App() {
+
   return (
+    <CarProvider>
     <div className="App">
     <Routes>
      <Route path='/'element={<Home/>}/>
+     <Route path='/gallery' element={<Gallery/>}/>
     </Routes>
     </div>
+    </CarProvider>
   );
 }
 
