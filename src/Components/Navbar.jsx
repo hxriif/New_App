fc4aaff24a8e28b6d01180b21a5ef184bad67acb
hxriif <zaidbnuarifc@gmail.com>
2024-05-20 11:58:45 +0530

    changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import {Heart,CarIcon, CarFront, Car, Caravan} from 'lucide-react'
 
 function Navbar() {
     const navigate=useNavigate()
@@ -12,7 +13,7 @@ function Navbar() {
   return (
     <div className='bg-white h-full flex flex-col items-center justify-center gap-4 w-full'>
               <div >
-                 <h1 className='font-bold italic font-great-vibes text-5xl'>Collectors </h1>
+                 <h1 className='font-bold italic font-great-vibes text-5xl flex items-center gap-3'>Find your<CarIcon strokeWidth={3}/></h1>
               </div>
               <div className='flex w-auto gap-7 font-thin text-sm'>
                 <button className='text-black hover:underline'>HOME</button>
