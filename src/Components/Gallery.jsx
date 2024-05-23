e5640b48c74e8515ae5d81984fe37c668a00da64
hxriif <zaidbnuarifc@gmail.com>
2024-05-23 10:08:39 +0530

    changes

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -5,24 +5,22 @@ import Navbar from './Navbar';
 
 function Gallery() {
   const { cars } = useContext(CarContext);
-  console.log(cars,'ashdfk');
+  console.log(cars, 'ashdfk');
 
   return (
-    <div className='h-screen w-screen   '>
-      <div className='grid md:grid-cols-2 sm:grid-cols-1 p-2 gap-2 bg-black '>
+    <div className='h-screen w-screen '>
+      <div className='grid md:grid-cols-2 sm:grid-cols-1 gap-4 bg-black '>
         {cars.map(car => (
-          <div key={car.id} className='bg-black rounded-lg  p-6 border border-slate-700  md:flex  gap-2'>
-            <div className='md:bg-cyan-800 p-1 h-48  rounded-lg  '>
+          <div key={car.id} className='relative md:rounded-lg border border-slate-700 overflow-hidden p-2  rounded  '>
             <img
               src={car.image}
               alt={car.model}
-              className='w-[40rem] h-full object-cover rounded-full md:rounded-lg sm:rounded-2xl mb-4  border md:border-none lg:border-none sm:border-none'
+              className='w-full h-64 object-cover rounded-lg'
             />
-            </div>
-            <div className=' w-full flex flex-col justify-center   h-48  p-1 rounded-lg '>
-            <h2 className='text-xl font-bold  text-white'>{car.make} {car.model}</h2>
-            <p className='text-white'>Year: {car.year}</p>
-            <button className='bg-blue-900 text-white p-1 rounded-lg mt-2'>show more</button>
+            <div className='absolute top-0 left-4 md:relative md:justify-center p-4 rounded-lg items-center'>
+              <h2 className=' font-bold text-yellow-500 text-2xl'>{car.make} {car.model}</h2>
+              <p className='text-red-600 font-semibold'>Year: {car.year}</p>
+              <button className='bg-blue-900 text-white p-2 rounded-lg mt-2'>Show more</button>
             </div>
           </div>
         ))}
